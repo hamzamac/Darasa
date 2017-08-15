ca5a8aedc626c51cc08b66b1a6404840622a48df
hamzamac <hamzamac@live.com>
2017-08-15 08:59:09 +0300

    setting the main view, hedder , body and footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './w3.css';
+
+class Footer extends Component
+{
+    render()
+    {
+        return 
+        (
+            <div className="">Footer</div>
+        );
+    }
+}
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './w3.css';
+
+
+class Header extends Component
+{
+    render()
+    {
+        return 
+        (
+            <div className=""><h1>Header</h1></div> 
+        );
+    }
+}
