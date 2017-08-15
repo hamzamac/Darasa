87fb2e7e510b62c07142e1cb1b86caf1ea99c282
hamzamac <hamzamac@live.com>
2017-08-15 14:43:32 +0300

    Added Vievwe.js Componets

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component
     render()
     {
         return (
-            <div className="w3-container w3-theme-dark"><a href="www.shewt.co.tz">
+            <div className="w3-container w3-theme-dark w3-center"><a href="www.shewt.co.tz">
                 Shewt 2017</a>
             </div>
         );
