a3523e2c9341f96f6b093183bb8d20e857c659b8
hamzamac <hamzamac@live.com>
2017-08-18 22:43:53 +0300

    Adding PDF viewer in the presentation

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
-
+import ReactPDF from 'react-pdf';
+import lecture from '../documents/Lecture_5.pdf';
 
 class Presentation extends Component
 {
+    onDocumentLoad({ total }) {
+        this.setState({ total });
+    }
+ 
+    onPageLoad({ pageIndex, pageNumber }) {
+        this.setState({ pageIndex, pageNumber });
+    }
     render()
     {
         return(
-            <div className="">
-              PDF presentation is loading ...
+            <div>
+                <ReactPDF
+                    file={lecture}
+                    pageIndex={2}
+                    onDocumentLoad={this.onDocumentLoad}
+                    onPageLoad={this.onPageLoad}
+                />
             </div>
         );
     }
