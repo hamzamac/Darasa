b6e16332fdeb38194b8d3f0f478deb2958898e20
hamzamac <hamzamac@live.com>
2017-08-30 11:49:34 +0300

    Added Page navigation functionality

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -6,7 +6,7 @@ class Presentation extends Component
 {
     constructor(props) {
         super(props);
-        this.state = {file: lecture};
+        this.state = {file: lecture,pageIndex:0};
     }
 
     message()
@@ -14,21 +14,27 @@ class Presentation extends Component
         alert("Clicked");
     }
 
+    changePage(by) {
+        this.setState(prevState => ({
+          pageIndex: prevState.pageIndex + by,
+        }));
+      }
+
     render()
     {
         return(
             <div className="w3-display-middle">
                 <ReactPDF
                     file={this.state.file}
-                    pageIndex={0}
+                    pageIndex={this.state.pageIndex}
                     scale={1.2}
                 />
 
                 <div className="w3-center">
                     <div class="w3-bar">
                         <button className="w3-button w3-theme" onClick={this.message}>{"<<"}</button>
-                        <button className="w3-button w3-theme">{"<"}</button>
-                        <button className="w3-button w3-theme">{">"}</button>
+                        <button className="w3-button w3-theme" onClick={() => this.changePage(-1)}>{"<"}</button>
+                        <button className="w3-button w3-theme" onClick={() => this.changePage(1)}>{">"}</button>
                         <button className="w3-button w3-theme">{">>"}</button>
                     </div>
                 </div>
