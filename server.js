1366812b57e574e4b21abfa4353f43e0eefaa39d
hamzamac <hamzamac@live.com>
2017-09-15 22:27:16 +0300

    addes socketio

--- server.js
+++ server.js
@@ -6,5 +6,7 @@ app.use(express.static('./build'));
 var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
-
+io.sockets.on('connection',(socket)=>{
+    console.log(socket.id);
+});
 console.log('listening...');
